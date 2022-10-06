fa5237ca15de893654ae3a4652bcb63ae22acd9e
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-10-06 18:28:31 +0300

    app theme refactored

--- app/src/main/java/com/composeweatherapp/app/theme/Theme.kt
+++ app/src/main/java/com/composeweatherapp/app/theme/Theme.kt
@@ -5,17 +5,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = TransparentDarkBlue,
+    onSurface = WhiteTransparent,
+    onSecondary = HighTransparentDarkBlue
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = White,
     primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = TransparentDarkBlue,
+    onSurface = WhiteTransparent,
+    onSecondary = HighTransparentDarkBlue
 
     /* Other default colors to override
     background = Color.White,
@@ -36,6 +43,12 @@ fun ComposeWeatherAppTheme(
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val systemUiController = rememberSystemUiController()
+
+    SideEffect {
+        systemUiController.setStatusBarColor(color = Color.Transparent)
     }
 
     MaterialTheme(
--- app/src/main/java/com/composeweatherapp/app/theme/Type.kt
+++ app/src/main/java/com/composeweatherapp/app/theme/Type.kt
@@ -1,28 +1,40 @@
 package com.composeweatherapp.app.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.composeweatherapp.R
 
-// Set of Material typography styles to start with
+val fonts = FontFamily(
+    Font(R.font.sourcesanspro_extralight, FontWeight.Light),
+    Font(R.font.sourcesansoro_semibold, FontWeight.SemiBold),
+    Font(R.font.sourcesanspro_regular, FontWeight.Normal)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+    h1 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        color = Color.White,
+        fontSize = 96.sp,
+        letterSpacing = 0.sp,
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+    h2 = TextStyle(
+        fontFamily = fonts,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        color = Color.White,
+        fontSize = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+        color = Color.White,
+        fontSize = 20.sp,
+        letterSpacing = 0.sp
+    ),
 )
