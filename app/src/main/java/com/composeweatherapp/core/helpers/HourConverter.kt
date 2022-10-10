fe0489141b33974f76cf160416a97c416453572c
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-10-10 15:59:19 +0300

    hour converter method added

--- app/src/main/java/com/composeweatherapp/core/helpers/HourConverter.kt
+++ app/src/main/java/com/composeweatherapp/core/helpers/HourConverter.kt
@@ -2,10 +2,20 @@ package com.composeweatherapp.core.helpers
 
 object HourConverter {
     fun convertHour(hour: String) : String {
-        return if(hour.toInt() <= 12) {
+        return if(hour.toInt() in 1..11) {
             "$hour AM"
         }else {
-            "${hour.toInt() - 12} PM"
+            when (hour) {
+                "00" -> {
+                    "12 AM"
+                }
+                "12" -> {
+                    "12 PM"
+                }
+                else -> {
+                    "0${hour.toInt() - 12} PM"
+                }
+            }
         }
     }
 }
