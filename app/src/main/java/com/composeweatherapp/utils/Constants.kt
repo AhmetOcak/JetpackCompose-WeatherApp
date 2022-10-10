8e0fc69e95a48055d249a241ec0fbfc33c42b244
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-10-10 15:57:42 +0300

    constants added

--- app/src/main/java/com/composeweatherapp/utils/Constants.kt
+++ app/src/main/java/com/composeweatherapp/utils/Constants.kt
@@ -2,12 +2,83 @@ package com.composeweatherapp.utils
 
 object NetworkService {
     const val BASE_URL: String = "https://api.openweathermap.org"
-    const val API_KEY: String = "YOUR API KEY"
+    const val API_KEY: String = "18fcba115a2815e21a21379f65b994ed"
     const val UNITS: String = "metric"
-    const val END_POINT = "/data/2.5/weather/"
+    const val CURRENT_WEATHER_END_POINT = "/data/2.5/weather/"
+    const val FORECAST_END_POINT = "/data/2.5/forecast"
+    const val UNIT = "metric"
 }
 
 object Database {
-    const val table_name = "weather_data"
+    const val current_weather_table = "current_weather_data"
+    const val forecast_table = "forecast_data"
     const val database_name = "weather_data.db"
+    const val city_table = "city_data"
+}
+
+object Constants {
+    const val UNKNOWN_ERROR = "An unknown error occurred."
+}
+
+object ExceptionTitles {
+    const val GPS_DISABLED = "GPS Disabled"
+    const val NO_PERMISSION = "No Permission"
+    const val NO_INTERNET_CONNECTION = "No Internet Connection"
+    const val UNKNOWN_ERROR = "Unknown Error"
+}
+
+object ExceptionDescriptions {
+    const val GPS_DISABLED_DESCR = "Your GPS seems to be disabled, please enable it."
+    const val NO_PERMISSION_DESCR = "Allow otherwise location tracking won't work."
+    const val NO_INTERNET_CONNECTION_DESCR = "Please check your internet connection."
+    const val UNKNOWN_ERROR_DESCR = "Something went wrong."
+}
+
+object ErrorCardConsts {
+    const val BUTTON_TEXT = "OK"
+}
+
+object AppStrings {
+
+    // HomeScreen -> ForecastSection
+    const val hourly_forecast = "Hourly Forecast"
+    const val daily_forecast = "Daily Forecast"
+
+    // HomeScreen -> WeatherDetailSection
+    const val temp = "🌡 TEMP"
+    const val feels_like = "🌡 FEELS LIKE"
+    const val cloudiness = "☁ CLOUDINESS"
+    const val humidity = "💧 HUMIDITY"
+    const val sunrise = "🌇 SUNRISE"
+    const val sunset = "🌆 SUNSET"
+    const val wind = "🌬 WIND"
+    const val metric = "KM"
+    const val pressure = "⏲ PRESSURE"
+    const val degree = "°"
+
+    // SearchCityScreen
+    const val topbar_title = "WeatherDto"
+
+    // SearchCityScreen -> SearchField
+    const val placeholder = "Search for a city"
+}
+
+object WeatherConditions {
+    const val CLEAR_SKY = "clear sky"
+    const val FEW_CLOUDS = "few clouds"
+    const val SCATTERED_CLOUDS = "scattered clouds"
+    const val BROKEN_CLOUDS = "broken clouds"
+    const val SHOWER_RAIN = "shower rain"
+    const val RAIN = "rain"
+    const val THUNDERSTORM = "thunderstorm"
+    const val SNOW = "snow"
+    const val MIST = "mist"
+}
+
+object MainWeatherConditions {
+    const val CLOUDS = "CloudsDto"
+    const val SNOW = "Snow"
+    const val RAIN = "Rain"
+    const val THUNDERSTORM = "Thunderstorm"
+    const val CLEAR = "Clear"
 }
