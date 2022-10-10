bb02a4377e2a483183896160fc65a9fde9f83ac3
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-10-10 15:52:17 +0300

    api ready

---
+++ app/src/main/java/com/composeweatherapp/data/datasource/remote/api/weatherapi/WeatherApi.kt
@@ -0,0 +1,23 @@
+package com.composeweatherapp.data.datasource.remote.api.weatherapi
+
+import com.composeweatherapp.data.datasource.remote.api.entity.ForecastDto
+import com.composeweatherapp.utils.NetworkService
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET(NetworkService.FORECAST_END_POINT)
+    suspend fun getForecastData(
+        @Query("lat") latitude: Double,
+        @Query("lon") longitude: Double,
+        @Query("APPID") apiKey: String = NetworkService.API_KEY,
+        @Query("units") units: String = NetworkService.UNITS,
+    ): ForecastDto
+
+    @GET(NetworkService.FORECAST_END_POINT)
+    suspend fun getForecastDataWithCityName(
+        @Query("q") cityName: String,
+        @Query("APPID") apiKey: String = NetworkService.API_KEY,
+        @Query("units") units: String = NetworkService.UNITS,
+    ): ForecastDto
+}
