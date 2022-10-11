26ef376d579bc446eb1523d042e4eed24ce2b718
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-10-11 22:43:21 +0300

    text align problem solved

--- app/src/main/java/com/composeweatherapp/presentation/component/ErrorCard.kt
+++ app/src/main/java/com/composeweatherapp/presentation/component/ErrorCard.kt
@@ -9,8 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.composeweatherapp.R
 
@@ -20,15 +20,11 @@ fun ErrorCard(
     errorTitle: String,
     errorDescription: String,
     errorButtonText: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    cardModifier: Modifier,
 ) {
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(LocalConfiguration.current.screenHeightDp.dp / 4 + 48.dp)
-                .padding(horizontal = 64.dp)
-        ) {
+        Card(modifier = cardModifier) {
             Column(
                 modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -65,6 +61,8 @@ fun ErrorCard(
                         verticalArrangement = Arrangement.SpaceAround
                     ) {
                         Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center ,
                             text = errorDescription,
                             style = MaterialTheme.typography.body1,
                             color = MaterialTheme.colors.primary
